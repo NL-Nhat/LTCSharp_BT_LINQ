fac193eb8088390b1865e17c73984e315745fd7c
NL-Nhat <nguyenlongnhat20050111@gmail.com>
2026-03-20 13:54:16 +0700

    bai2

---
+++ Bai2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=========================================");
+            Console.WriteLine("NGUYEN LONG NHAT - 23115053122126-23T1");
+            Console.WriteLine("=========================================\n");
+
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            var list = numbers.Where(x => x > 5).ToList();
+
+            Console.Write("Cac so > 5 la: ");
+
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine("\n");
+        }
+    }
+}
