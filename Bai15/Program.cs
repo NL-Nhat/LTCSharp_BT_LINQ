feb86822c67a9167680c26c0640ad6db9cb39044
NL-Nhat <nguyenlongnhat20050111@gmail.com>
2026-03-20 22:28:10 +0700

    bai15

---
+++ Bai15/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=======================================");
+            Console.WriteLine("NGUYEN LONG NHAT-23115053122126-23T1");
+            Console.WriteLine("=======================================\n");
+
+            List<int> numbers = new List<int>() { 1, 2, 2, 3, 4, 4, 5 };
+
+            Console.Write("Danh sach ban dau: ");
+
+            foreach (var number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+
+            var uniques = numbers.Distinct();
+
+            Console.Write("\nDanh sach khong trung: ");
+
+            foreach (var number in uniques)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
