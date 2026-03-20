d1c2689e1681952d6774c687e9407c954c3f27e2
NL-Nhat <nguyenlongnhat20050111@gmail.com>
2026-03-20 22:32:41 +0700

    bai20

---
+++ Bai20/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai20
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Score { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=======================================");
+            Console.WriteLine("NGUYEN LONG NHAT-23115053122126-23T1");
+            Console.WriteLine("=======================================\n");
+
+            List<Student> students = new List<Student>() {
+                 new Student { Id = 1, Name = "An", Score = 8 },
+                 new Student { Id = 2, Name = "Binh", Score = 6 },
+                 new Student { Id = 3, Name = "Chi", Score = 9 },
+                 new Student { Id = 4, Name = "Dung", Score = 7 }
+            };
+
+            Console.WriteLine("Danh sach sinh vien:");
+
+            foreach (var student in students)
+            {
+                Console.WriteLine($"Id: {student.Id}, Name: {student.Name}, Score: {student.Score}");
+            }
+
+            var sv = students.OrderByDescending(s => s.Score).Take(3);
+
+            Console.WriteLine("\nDanh sach 3 sinh vien co diem cao nhat:");
+
+            foreach (var student in sv)
+            {
+                Console.WriteLine($"Id: {student.Id}, Name: {student.Name}, Score: {student.Score}");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
