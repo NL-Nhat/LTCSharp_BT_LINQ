efca520a3fa59bac2a248bf812ad8a36258eb065
NL-Nhat <nguyenlongnhat20050111@gmail.com>
2026-03-20 22:30:15 +0700

    bai17

---
+++ Bai17/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai17
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Score { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=======================================");
+            Console.WriteLine("NGUYEN LONG NHAT-23115053122126-23T1");
+            Console.WriteLine("=======================================\n");
+
+            List<Student> students = new List<Student>() {
+                new Student { Id = 1, Name = "An", Score = 8 },
+                new Student { Id = 2, Name = "Binh", Score = 6 },
+                new Student { Id = 3, Name = "Chi", Score = 9 },
+                new Student { Id = 4, Name = "Dung", Score = 7 }
+            };
+
+            Console.WriteLine("Danh sach sinh vien:");
+
+            foreach (var student in students)
+            {
+                Console.WriteLine($"Id: {student.Id}, Name: {student.Name}, Score: {student.Score}");
+            }
+
+            double diemtb = students.Average(s => s.Score);
+
+            Console.WriteLine($"\nDiem trung binh cua sinh vien la: {diemtb:F2}");
+
+            Console.WriteLine();
+        }
+    }
+}
