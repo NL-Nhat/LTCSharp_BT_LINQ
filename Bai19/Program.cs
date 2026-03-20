ba59c3dfcd516e25c8eb2bc06cc43f180e42a65e
NL-Nhat <nguyenlongnhat20050111@gmail.com>
2026-03-20 22:31:52 +0700

    bai19

---
+++ Bai19/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=======================================");
+            Console.WriteLine("NGUYEN LONG NHAT-23115053122126-23T1");
+            Console.WriteLine("=======================================\n");
+
+            List<int> numbers = new List<int>() { 1, 1, 2, 3, 4, 4, 7, 4, 9 };
+
+            Console.Write("Danh sach ban dau: ");
+
+            foreach (int number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+
+            var so = numbers.GroupBy(n => n)
+                            .OrderByDescending(g => g.Count())
+                            .First()
+                            .Key;
+
+            Console.Write($"\nSo xuat hien nhieu nhat la: {so}");
+
+            Console.WriteLine();
+        }
+    }
+}
