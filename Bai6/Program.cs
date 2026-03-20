3eb7cd7c99242d41938901c43ee52d8c187f9d53
NL-Nhat <nguyenlongnhat20050111@gmail.com>
2026-03-20 14:01:51 +0700

    bai6

---
+++ Bai6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=========================================");
+            Console.WriteLine("NGUYEN LONG NHAT - 23115053122126-23T1");
+            Console.WriteLine("=========================================\n");
+
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            Console.WriteLine("Danh sach ban dau: " + string.Join(", ", numbers));
+
+            var list = numbers.Where(n => n % 2 == 0);
+
+            Console.Write("Cac so chan trong sanh sach: ");
+
+            foreach (var number in list)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
