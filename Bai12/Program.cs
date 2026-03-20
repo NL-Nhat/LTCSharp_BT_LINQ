58b68fa10da3af126363395de075241a5149817d
NL-Nhat <nguyenlongnhat20050111@gmail.com>
2026-03-20 22:25:29 +0700

    bai12

---
+++ Bai12/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai12
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Score { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=======================================");
+            Console.WriteLine("NGUYEN LONG NHAT-23115053122126-23T1");
+            Console.WriteLine("=======================================\n");
+
+            List<Student> students = new List<Student>() {
+                new Student { Id = 1, Name = "An", Score = 8 },
+                new Student { Id = 2, Name = "Binh", Score = 6 },
+                new Student { Id = 3, Name = "Chi", Score = 9 },
+                new Student { Id = 4, Name = "Dung", Score = 7 }
+            };
+
+            int dem = students.Count(s => s.Score >= 7);
+
+            Console.WriteLine($"So luong sinh vien co diem >= 7 la: {dem}");
+
+            Console.WriteLine();
+        }
+    }
+}
